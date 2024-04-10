2dbc847be5ebe58d43016b89bf32f6c2c4aae847
Ashwin <ashwin.s@gmail.com>
2024-04-10 17:02:07 +0530

    fix: moved API key to env file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCZ8vgnxD3JM8c0l43jJsP88Z7ScCuYftg";
+const GOOGLE_API_KEY = process.env["REACT_APP_GOOGLE_API_KEY"];
 export const LIVE_CHAT_COUNT = 25;
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=60&regionCode=IN&key=" +
